15e32b67abf40727df957326793e62b6b2422d78
Thongramdhanapyari <thongramdhanapyari@gmail.com>
2026-04-14 13:20:33 +0530

    fix python version

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, Field
-from typing import Optional
+from typing import Optional, ClassVar
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -7,4 +7,4 @@ class User(SQLModel, table=True):
     email: str = Field(index=True)
     password: str
 
-    __table_args__ = {"sqlite_autoincrement": True}
+    __table_args__: ClassVar[dict] = {"sqlite_autoincrement": True}
